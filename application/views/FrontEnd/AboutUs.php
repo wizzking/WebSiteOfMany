9a62a196b06d757d89d67cb1808dd9208c17c896
wizzking <cotafiesta@gmail.com>
2018-03-08 23:29:47 -0600

    tom

--- application/views/FrontEnd/AboutUs.php
+++ application/views/FrontEnd/AboutUs.php
@@ -1,3 +1,32 @@
 <?php $this->load->view('FrontEnd/Global/Header'); ?>
-asdas
+	<link rel="stylesheet" type="text/css" href="../../assets/sources/css/TomEliezer.css">
+	<div class="container header" id="header-aboutUs">
+		<img id="img-header-AboutUS"  src="../../assets/sources/img/ImgHeaderAboutUS.jpg">
+	</div>
+	<div class="container ourHistory">
+			<h2><strong>Our History</strong></h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+	<div class="container ourHotel" id="ourHotel">
+		<video src="../../assets/sources/video/ourHotel.mp4" loop controls>
+		</video>
+		<p>OUR HOTEL VIDEO</p>
+	</div>
+	<div class="container ourCooks" id="ourCooks">
+		<div class="ourCooks-gallery">
+			    <img src="../../assets/sources/img/prueba.png"  >
+			  <div class="ourCooks-desc ">Chef.JONH CLIFE</div>
+		</div>
+		<div class="ourCooks-gallery">
+			    <img src="../../assets/sources/img/giphy.gif"  >
+			  <div class="ourCooks-desc ">Chef.JUAN CHOSE</div>
+		</div>		
+	</div>
+
+
 <?php $this->load->view('FrontEnd/Global/Footer'); ?>
