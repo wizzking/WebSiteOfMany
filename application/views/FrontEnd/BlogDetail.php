a613e0e898ce2cc0c240a83d11bed1eb053b455e
piluman <2015030704@upsin.edu.mx>
2018-03-12 08:29:33 -0600

    blogdetails

--- application/views/FrontEnd/BlogDetail.php
+++ application/views/FrontEnd/BlogDetail.php
@@ -1,3 +1,56 @@
 <?php $this->load->view('FrontEnd/Global/Header'); ?>
+	<link rel="stylesheet" type="text/css" href="../../assets/sources/css/pilu.css">
+	<div class="header" id="blogDetail">
+		<img src="../../assets/sources/img/imgHeaderBlogDetail4.jpg">
+		<img src="../../assets/sources/img/niu.png" id="img">
+	</div>
+	<br><br>
 
+	<div class="col-md-6 col-md-offset-2 container-fluid" >
+		<div class="col-md-10" id="image">
+			<img src="../../assets/sources/img/imgHeaderBlogDetail5.jpg">
+		</div>
+		
+			<div class="col-md-2">
+				<ul>
+					<h3><strong>Categorias</strong></h3>
+					<li>ALL FOOD</li>
+					<li>VEGETABLES</li>
+					<li>DELICIOUS FOOD</li>
+					<li>SPECIAL DISHES</li>
+					<li>JUICES</li>	
+				</ul>
+
+			</div>
+			<div class="col-md-2">
+				<ul>
+					<h3><strong>RecentPost</strong></h3>
+					<li><img src="../../assets/sources/img/img70.jpg"></li>
+				</ul>
+			</div>
+	</div>
+	<div class="col-md-5 col-md-offset-2 container-fluid">
+		<div class="row">
+			<div class="col-md-12 " id="img2">
+				<h3><strong>Aenca pharetra leetus utsto</strong></h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-5 col-md-offset-2 container-fluid">
+		<div class="row">
+			<div>
+				<ul class="pager">
+		  			<li><a href="#">Previous</a></li>
+		  			<li><a href="#">Next</a></li>
+				</ul>
+
+			</div>
+		</div>
+	</div>
 <?php $this->load->view('FrontEnd/Global/Footer'); ?>
