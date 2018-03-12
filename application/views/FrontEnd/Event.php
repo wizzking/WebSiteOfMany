76139cc9f07dd240b48a9b12188bd682b31ce99f
ereo <34866827+ereo@users.noreply.github.com>
2018-03-12 00:27:35 -0700

    PrbEr

--- application/views/FrontEnd/Event.php
+++ application/views/FrontEnd/Event.php
@@ -1,3 +1,181 @@
 <?php $this->load->view('FrontEnd/Global/Header'); ?>
 
+
+
+<div class="container" style="max-height: 250px;max-width: 1970px;width: 100%;">
+	<div class="row" >
+
+		<div id="imagenes" style=" background: #7d8587;">
+
+			<div id="contimagen">
+				<img  src="../../assets/sources/img/banner.png"></img>
+			</div>
+
+		</div>
+
+	</div>
+</div>
+<br>
+<div class="container" style=" width: 100%;">
+
+		<div class="row">
+			<div class="col-md-1" ></div>
+			<div class="col-md-1" ></div>
+			<div class="col-md-1" >
+
+						<div  style="border: 10px; border-top: 10px solid #fff; width: 80px; background: orange; text-align: center;
+  border-right: 10px solid #fff;
+  border-bottom: 10px solid #fff;
+  border-left: 10px solid #fff;
+  border-style: double ;
+  border-color: #fff; margin-top: 5px; margin-bottom: 1px; margin-right: 1px; margin-left: 3px;">
+							<label>08</label>
+							<p>NOV</p>
+							<p>2016</p>
+						</div>
+			</div>	
+
+			<div class="col-md-3" id="contimagen" style="height: 340px;width: 590px; ">
+				<img  src="../../assets/sources/img/post.jpg" ></img>
+			</div>
+
+			<div class="col-md-3">
+				<label id="eventhead">Event Heading Here</label>
+				<br>
+				<label id="eventtime">7.00 pm - 10.00 pm</label>
+				<br>
+				<p id="eventcontent">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<br>
+				<br>
+				<button type="button" class="btn btn-lg btn-primary" disabled="disabled" style="background: #ffeac5; border: 0; color: #e48800; ">VIEW EVENT DETAILS</button>
+			</div>
+
+		</div>
+		<br>
+		<div class="row">
+			<div class="col-md-1" ></div>
+			<div class="col-md-1" ></div>
+			<div class="col-md-1" >
+				<div  style="border: 10px; border-top: 10px solid #fff; width: 80px; background: orange; text-align: center;
+  border-right: 10px solid #fff;
+  border-bottom: 10px solid #fff;
+  border-left: 10px solid #fff;
+  border-style: double ;
+  border-color: #fff; margin-top: 5px; margin-bottom: 1px; margin-right: 1px; margin-left: 3px;">
+					<label>08</label>
+					<p>NOV</p>
+					<p>2016</p>
+				</div>
+			</div>	
+
+			<div class="col-md-3" id="contimagen" style="height: 340px;width: 590px; ">
+				<img  src="../../assets/sources/img/post.jpg" ></img>
+			</div>
+
+			<div class="col-md-3">
+				<label id="eventhead">Event Heading Here</label>
+				<br>
+				<label id="eventtime">7.00 pm - 10.00 pm</label>
+				<br>
+				<p id="eventcontent">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<br>
+				<br>
+				<button type="button" class="btn btn-lg btn-primary" disabled="disabled" style="background: #ffeac5; border: 0; color: #e48800; ">VIEW EVENT DETAILS</button>
+			</div>
+
+		</div>
+		<br>
+		<div class="row">
+			<div class="col-md-1" ></div>
+			<div class="col-md-1" ></div>
+			<div class="col-md-1" >
+				<div  style="border: 10px; border-top: 10px solid #fff; width: 80px; background: orange; text-align: center;
+  border-right: 10px solid #fff;
+  border-bottom: 10px solid #fff;
+  border-left: 10px solid #fff;
+  border-style: double ;
+  border-color: #fff; margin-top: 5px; margin-bottom: 1px; margin-right: 1px; margin-left: 3px;">
+					<label>08</label>
+					<p>NOV</p>
+					<p>2016</p>
+				</div>
+			</div>	
+
+			<div class="col-md-3" id="contimagen" style="height: 340px;width: 590px; ">
+				<img  src="../../assets/sources/img/post.jpg" ></img>
+			</div>
+
+			<div class="col-md-3">
+				<label id="eventhead">Event Heading Here</label>
+				<br>
+				<label id="eventtime">7.00 pm - 10.00 pm</label>
+				<br>
+				<p id="eventcontent">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<br>
+				<br>
+				<button type="button" class="btn btn-lg btn-primary" disabled="disabled" style="background: #ffeac5; border: 0; color: #e48800; ">VIEW EVENT DETAILS</button>
+			</div>
+
+		</div>
+
+</div>
+<br>
+<div class="container" style=" background: #ffffff; width: 100%; margin-top: 0%">
+	<div class="row">
+
+		<ul class="pager">
+		  <li ><a href="#" style="background: orange; color: #ffffff"  >< NEXT</a></li>
+		  <li><a href="#" style="color: black;">1-3</a></li>
+		  <li><a href="#" style="color: black;">3-9</a></li>
+		  <li><a href="#" style="color: black;">9-12</a></li>
+		  <li><a href="#" style="background: #ffeac5; color: #fea100">PREV ></a></li>
+		</ul>
+		
+	</div>
+</div>
+<br>
+<div class="container" style=" background: #edf1f3; width: 100%; margin-top: 0%">
+	<div class="row">
+
+
+			<div class="col-md-4" style="text-align: right;">
+				<label>Opening Hours</label>
+			</div>
+
+			<div class="col-md-4" style="text-align: center;">
+				<label>Monday to Friday</label>
+				<br>
+				<label style="color: orange;">10am - 10pm</label>
+			</div>
+
+			<div class="col-md-4" style="text-align: left;">
+				<label>Saturday to Sunday</label>
+				<br>
+				<label style="color: orange;" >09am - 11pm</label>
+			</div>
+
+	</div>
+</div>
+
 <?php $this->load->view('FrontEnd/Global/Footer'); ?>
