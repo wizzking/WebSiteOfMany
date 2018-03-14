6d8ab2674e88abbc32b9c83cceb710c4a0ccf578
WizzKing <cotafiesta@gmail.com>
2018-03-13 20:21:03 -0700

    tom&pilu

--- application/views/FrontEnd/BlogDetail.php
+++ application/views/FrontEnd/BlogDetail.php
@@ -5,15 +5,15 @@
 		<img src="../../assets/sources/img/niu.png" id="img">
 	</div>
 	<br><br>
-
-	<div class="col-md-6 col-md-offset-2 container-fluid" >
-		<div class="col-md-10" id="image">
-			<img src="../../assets/sources/img/imgHeaderBlogDetail5.jpg">
+	<div class="row">
+		<div class="col-md-5 col-md-offset-2 container-fluid" >
+			<div class="col-md-10" id="image">
+				<img src="../../assets/sources/img/imgHeaderBlogDetail5.jpg">
+			</div>
 		</div>
-		
-			<div class="col-md-2">
+			<div class="col-md-3">
 				<ul>
-					<h3><strong>Categorias</strong></h3>
+					<h3><strong>Category</strong></h3>
 					<li>ALL FOOD</li>
 					<li>VEGETABLES</li>
 					<li>DELICIOUS FOOD</li>
@@ -22,35 +22,46 @@
 				</ul>
 
 			</div>
-			<div class="col-md-2">
-				<ul>
-					<h3><strong>RecentPost</strong></h3>
-					<li><img src="../../assets/sources/img/img70.jpg"></li>
-				</ul>
-			</div>
-	</div>
-	<div class="col-md-5 col-md-offset-2 container-fluid">
-		<div class="row">
-			<div class="col-md-12 " id="img2">
-				<h3><strong>Aenca pharetra leetus utsto</strong></h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			</div>
-		</div>
-	</div>
-	<div class="col-md-5 col-md-offset-2 container-fluid">
-		<div class="row">
-			<div>
-				<ul class="pager">
-		  			<li><a href="#">Previous</a></li>
-		  			<li><a href="#">Next</a></li>
-				</ul>
-
-			</div>
-		</div>
+				<div class="col-md-5 lista">	
+						<h3><strong>Recent Post</strong></h3>
+							<div class="col-md-6 lista">
+								<img src="../../assets/sources/img/img70.jpg">
+								<label>08-Nov-2016</label>
+								<p>Suspendisse eu dui libero </p>
+						</div>
+							
+						<div class="col-md-7 lista">
+							<br>
+							<img src="../../assets/sources/img/img70.jpg">
+							<label>13-Nov-2016</label>
+							<p>Quisque pellentes turpis si amet</p>
+						</div>
+						
+						<div class="col-md-8 lista">
+							<br>
+							<img src="../../assets/sources/img/img70.jpg">
+							<label>20-Nov-2016</label>			
+							<p>Suspendisse eu dui libero malesuada</p>
+						</div>
+				</div>
+				<div class="col-md-5 col-md-offset-2 ">			
+					<div>
+						<h3><strong>Aenca pharetra leetus utsto</strong></h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+				</div>
+				<div class="col-md-5 col-md-offset-2 container-fluid">
+						<div>
+							<ul class="pager">
+					  			<li><a href="#">Previous</a></li>
+					  			<li><a href="#">Next</a></li>
+							</ul>
+						</div>
+				</div>	
 	</div>
 <?php $this->load->view('FrontEnd/Global/Footer'); ?>
--- assets/sources/js/TomEliezer.js
+++ assets/sources/js/TomEliezer.js
@@ -1,30 +1,30 @@
 $(document).ready(function(){
 	$('#optionBreakfast').click(function(){
-		$('#FoodOptions > .pizza , .soup , .lunch, .snack , .dinner').hide(500);
-		$('#FoodOptions > .breakfast').show(500);
+		$('.menu-gallery').hide(500);
+		$('.breakfast').show(500);
 	});
 	$('#optionLunch').click(function(){
-		$('#FoodOptions > .pizza , .soup , .breakfast, .snack , .dinner').hide(500);
-		$('#FoodOptions > .lunch').show(500);
+		$('.menu-gallery').hide(500);
+		$('.lunch').show(500);
 	});
 	$('#optionSnacks').click(function(){
-		$('#FoodOptions > .pizza , .soup , .breakfast, .lunch , .dinner').hide(500);
-		$('#FoodOptions > .snack').show(500);
+		$('.menu-gallery').hide(500);
+		$('.snack').show(500);
 	});
 	$('#optionPizza').click(function(){
-		$('#FoodOptions > .snack , .soup , .breakfast, .lunch , .dinner').hide(500);
-		$('#FoodOptions > .pizza').show(500);
+		$('.menu-gallery').hide(500);
+		$('.pizza').show(500);
 	});
 	$('#optionSoups').click(function(){
-		$('#FoodOptions > .snack , .pizza , .breakfast, .lunch , .dinner').hide(500);
-		$('#FoodOptions > .soup').show(500);
+		$('.menu-gallery').hide(500);
+		$('.soup').show(500);
 	});
 	$('#optionDinner').click(function(){
-		$('#FoodOptions > .snack , .pizza , .breakfast, .lunch , .soup').hide(500);
-		$('#FoodOptions > .dinner').show(500);
+		$('.menu-gallery').hide(500);
+		$('.dinner').show(500);
 	});
 	$('#optionAll').click(function(){
-		$('#FoodOptions > .pizza , .soup , .lunch, .snack , .dinner , .breakfast').show(500);
+		$('.pizza , .soup , .lunch, .snack , .dinner , .breakfast').show(500);
 	});			
 		
 });
