7957953d3f7cff46d4b56dd1032a1cb7abbb26db
daniel <2015030282@upsin.edu.mx>
2018-03-15 21:53:49 -0700

    la vista de mirtha aceptala vato

--- application/views/FrontEnd/Gallery.php
+++ application/views/FrontEnd/Gallery.php
@@ -1,3 +1,69 @@
 <?php $this->load->view('FrontEnd/Global/Header'); ?>
+	<div class="header gallery" id="header-gallery">
+		<img id="img-header-Gallery"  src="../../assets/sources/img/Mirtha/gallery.jpg" width="100%" height="100%">
+		<img id="img-header-Gallery"  src="../../assets/sources/img/Mirtha/newgallery.png" width="30%" height="30%" style="position: absolute; top:10%; left: 35%">
+	</div>
 
-<?php $this->load->view('FrontEnd/Global/Footer'); ?>
+
+	
+ <div class="container ourGallery" style="font-size: 12px; position: relative; text-align: center;">
+ 	 <h2><strong>Our Gallery</strong></h2>
+
+
+ </div>
+ 	<div >
+ 		<ul class="img-gal" >
+ 		<img id="img-iconos" src="../../assets/sources/img/Mirtha/photos.png" style="font-size: 12px; position: relative; left:39% ">
+ 		<img id="img-iconos"  src="../../assets/sources/img/Mirtha/restaurant.png" style="font-size: 12px; position: relative; left:43.5% ">
+ 		<img id="img-iconos"  src="../../assets/sources/img/Mirtha/food.png" style="font-size: 12px; position: relative; left:46.5% ">
+ 		<img id="img-iconos"  src="../../assets/sources/img/Mirtha/desserts.png" style="font-size: 12px; position: relative; left:49% ">
+ 		
+ 		</ul>
+ 	</div>
+
+<div >
+ <ul class="nav navbar-nav" style="font-size: 12px; position: relative; left:39% ;">
+      <li class="active"><a href="#" >All Photos</a></li>
+      <li><a href="#" style="font-size: 12px;">Restaurant</a></li>
+      <li><a href="#" style="font-size: 12px;">Food</a></li>
+      <li><a href="#" style="font-size: 12px;">Desserts</a></li>
+     
+    </ul>
+    </div>
+
+    <div >
+    <ul  class="img-din">
+      <img id="img-dinamica" src="../../assets/sources/img/Mirtha/img.png" style="font-size: 12px; position: relative; top:50%;left:25% ">
+      <img id="img-dinamica" src="../../assets/sources/img/Mirtha/img.png" style="font-size: 12px; position: relative; top:50%;left:35% " >
+    </ul>
+    </div>
+
+    <div>
+      <ul class="img-din">
+         <img id="img-dinamica" src="../../assets/sources/img/Mirtha/img.png" style="font-size: 12px; position: relative; top:100%;left:25% " >
+        <img id="img-dinamica" src="../../assets/sources/img/Mirtha/img.png" style="font-size: 12px; position: relative; top:100%;left:35% " >
+      </ul>
+    </div>
+
+     <div >
+      <ul class="img-din">
+         <img id="img-dinamica" src="../../assets/sources/img/Mirtha/img.png" style="font-size: 12px; position: relative; top:100%;left:25% " >
+        <img id="img-dinamica" src="../../assets/sources/img/Mirtha/img.png" style="font-size: 12px; position: relative; top:100%;left:35% " >
+      </ul>
+    </div>
+
+  <div class="container col-md-6 col-md-offset-3" style="margin-bottom: 10%; ">                
+  <ul class="pagination">
+    <li><a href="#" style="background-color: #e2ad56">1</a></li>
+    <li><a href="#" style="background-color: #e2ad56">2</a></li>
+    <li><a href="#" style="background-color: #e2ad56">3</a></li>
+    <li><a href="#" style="background-color: #e2ad56">4</a></li>
+    <li><a href="#" style="background-color: #e2ad56">5</a></li>
+  </ul>
+</div>
+
+
+    <?php $this->load->view('FrontEnd/Global/Footer'); ?>
+
+
+
