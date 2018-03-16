a0cd2f74a7e4f5c8dea396f66efaf51f5a0fdeaa
SheylaMedina <2015030384@upsin.edu.mx>
2018-03-16 08:41:18 -0700

    RecipiesDetails

--- application/views/FrontEnd/RecipiesDetail.php
+++ application/views/FrontEnd/RecipiesDetail.php
@@ -2,26 +2,180 @@
 
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/sources/css/Sheyla.css');?>">
 
+		<div class="header">
+		<img src="<?php echo base_url('assets/sources/img/detalles.jpg'); ?>" width="400px" heigh="1000px">
+	
+				<!--<h1> <img src="<?php echo base_url('assets/sources/img/florderecha.png'); ?>" width="50px" heigh="50px"> Detalles de la Receta <img src="<?php echo base_url('assets/sources/img/florizquierda.png'); ?>" width="50px" heigh="50px"> </h1>-->
+		</div>
 
-	<div class="EncabezadoRecipies">
+
+
+<section class="row">
+	<div class="container">
 		<div>
-			<p>IMAGEN</p>
+			<div class="recipie">
+				<br>
+				<label id="nombreReceta">PAKODA CHATNY</label><br>
+				<img src="<?php echo base_url('assets/sources/img/estrellas.png'); ?>" width="100px" heigh="100px">
+			</div>
 		</div>
-		<h1> <img src="<?php echo base_url('assets/sources/img/florderecha.png'); ?>" width="50px" heigh="50px"> Detalles de la Receta <img src="<?php echo base_url('assets/sources/img/florizquierda.png'); ?>" width="50px" heigh="50px"> </h1>
+	</div>
+</section>
 
-		</div>
+<section class="row">
+	<div class="container">
+			<div class="col-xs-6 col-xs-offset-3">
+				<img src="<?php echo base_url('assets/sources/img/platillo.png'); ?>" width="500px" heigh="200px">
+					<div class="row ">
+						<div class="col-md-4">
+							<p><strong>15 M</strong></p> 
+							<p>Tiempo de preparación</p>
+						</div>
 
-		<div>
-			<h4>PAKODA CHATNY</h4>
-			<img src="<?php echo base_url('assets/sources/img/estrellas.png'); ?>" width="100px" heigh="100px">
-		</div>
+						<div class="col-md-4">
+							<p><strong>30 M</strong></p> 
+							<p>Tiempo de cocción</p>
+						</div>
 
-		<div class="DivImagen">
-			<p>IMAGEN</p>
+						<div class="col-md-4">
+							<p>Download
+							<img src="<?php echo base_url('assets/sources/img/pdf.png'); ?>" width="50px" heigh="50px"></p>
+						</div>
+					</div>
+			</div>
 		</div>
+</section>
+
+<section class="row">
+	<div class="container">
+		<div class="container text-muted">
+		<blockquote>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</blockquote>
+		</div>
+	</div>
+</section>
+
+<section class="row">
+	<div class="container">
+		<div class="col-xs-6 col-xs-offset-3">
+			<p id="ingredientes">Ingredientes</p> <br>
+			<p><img src="<?php echo base_url('assets/sources/img/check.png'); ?>" width="50px" heigh="50px"> 1/2 taza de limón</p>
+			<p><img src="<?php echo base_url('assets/sources/img/check.png'); ?>" width="50px" heigh="50px"> 1/2 taza de limón</p>
+			<p><img src="<?php echo base_url('assets/sources/img/check.png'); ?>" width="50px" heigh="50px"> 1/2 taza de limón</p>
+			<p><img src="<?php echo base_url('assets/sources/img/check.png'); ?>" width="50px" heigh="50px"> 1/2 taza de limón</p>
+			<p><img src="<?php echo base_url('assets/sources/img/check.png'); ?>" width="50px" heigh="50px"> 1/2 taza de limón</p>
+		</div>
+	</div>
+</section>
+
+<section class="row">
+	<div>
 		<img src="<?php echo base_url('assets/sources/img/fondoReceta.png'); ?>" width="100%" heigh="100px">
+	</div>
+</section>
+
+<section class="row">
+			<h2>Recetas presentadas</h2>
+
+			<div class="container">
+				<div class="col-md-4">
+					<div class="col-xs-12 FeaturedRecipies" >
+						<label>hola</label>
+					</div>
+				
+			</div>
+		
+			<div class="col-md-4 ">
+				<div class="col-xs-12 FeaturedRecipies" >
+						<label>hola</label>
+					</div>
+			</div>
+
+			<div class="col-md-4 ">
+				<div class="col-xs-12 FeaturedRecipies" >
+						<label>hola</label>
+					</div>
+			</div>	
+
+			</div>
+			
+</section>
 
 
 
+<!--
+<div>
+		<div class="EncabezadoRecipies">
+			<div>
+				<p>IMAGEN</p>
+			</div>
+				<h1> <img src="<?php echo base_url('assets/sources/img/florderecha.png'); ?>" width="50px" heigh="50px"> Detalles de la Receta <img src="<?php echo base_url('assets/sources/img/florizquierda.png'); ?>" width="50px" heigh="50px"> </h1>
+		</div><br><br>
 
+		<div>
+			<div class="recipie">
+				<h4>PAKODA CHATNY</h4>
+				<img src="<?php echo base_url('assets/sources/img/estrellas.png'); ?>" width="100px" heigh="100px">
+			</div>
+		</div>
+
+		<div>
+		<div style="margin-top: 5%;">
+			<div class="prueba">
+				<p>IMAGEN</p>
+			</div>
+		</div>
+		</div>
+
+		<div class="container Ingredientes">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+
+		<div>
+			<label>INGREDIENTES</label> <br>
+			<img src="<?php echo base_url('assets/sources/img/check.png'); ?>" width="50px" heigh="50px"><br><br>
+			<img src="<?php echo base_url('assets/sources/img/check.png'); ?>" width="50px" heigh="50px"><br><br>
+			<img src="<?php echo base_url('assets/sources/img/check.png'); ?>" width="50px" heigh="50px"><br><br>
+			<img src="<?php echo base_url('assets/sources/img/check.png'); ?>" width="50px" heigh="50px"><br><br>
+		</div>
+
+		<div style="margin-top: 30%;">
+			<img src="<?php echo base_url('assets/sources/img/fondoReceta.png'); ?>" width="100%" heigh="100px">
+		</div>
+
+	  <div>
+			<h2>Recetas presentadas</h2>
+			<div class="FeaturedRecipies1">
+				<label>hola</label>
+			</div>
+		
+			<div class="FeaturedRecipies2">
+				<label>ya me enfade</label>
+			</div>
+
+			<div class="FeaturedRecipies3">
+				<label>Yaaaa</label>
+			</div>
+		</div>
+	
+
+		<div class="container openingHours">
+			<h3 >Opening Hours</h3>
+			<p><small>Monday to Friday</small><br/>10am-10pm</p>
+			<p><small>Saturday to Sunday</small><br/>9am-11pm</p>
+		</div>
+</div>
+
+-->
 <?php $this->load->view('FrontEnd/Global/Footer'); ?>
