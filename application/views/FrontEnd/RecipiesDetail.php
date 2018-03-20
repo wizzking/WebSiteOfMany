6d7f160e7e3141f15dc7ca6dfa96159663716137
SheylaMedina <2015030384@upsin.edu.mx>
2018-03-20 14:00:49 -0700

    Recipies

--- application/views/FrontEnd/RecipiesDetail.php
+++ application/views/FrontEnd/RecipiesDetail.php
@@ -1,10 +1,11 @@
 <?php $this->load->view('FrontEnd/Global/Header'); ?>
 
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/sources/css/Sheyla.css');?>">
-	<div class="header">
+
+<div class="header">
 		<img src="<?php echo base_url('assets/sources/img/detalles.png'); ?>" width="400px" heigh="1000px">	
 	</div>
-
+<div class="container">
 <section class="row">
 	<div class="container">
 		<div>
@@ -18,9 +19,9 @@
 </section>
 
 <section class="row">
-	<div class="container panel panel-default back">
+	<div class="container">
 			<div class="col-xs-6 col-xs-offset-3">
-				<img src="<?php echo base_url('assets/sources/img/platillo.png'); ?>" width="500px" heigh="200px">
+				<img src="<?php echo base_url('assets/sources/img/platillo.jpg'); ?>" width="100%" heigh="100%">
 					<div class="row  panel panel-default datosImagen">
 						<div class="col-md-4">
 							<p><strong>15 M</strong></p> 
@@ -59,8 +60,7 @@
 
 <section class="row">
 	<div class="container">
-		<div class="col-md-5 panel-body">
-			<div class="col-xs-12" >
+			<div >
 				<p id="ingredientes">Ingredientes</p> <br>
 				<p><img src="<?php echo base_url('assets/sources/img/check.png'); ?>" width="50px" heigh="50px"> 1/2 taza de limón</p>
 				<p><img src="<?php echo base_url('assets/sources/img/check.png'); ?>" width="50px" heigh="50px"> 1/2 taza de limón</p>
@@ -69,36 +69,8 @@
 				<p><img src="<?php echo base_url('assets/sources/img/check.png'); ?>" width="50px" heigh="50px"> 1/2 taza de limón</p>
 			</div>
 		</div>
-
-		<div class="col-md-6 panel-body">
-			<div class="col-xs-12 Nutrition" >
-				<p id="ingredientes">Nutrición</p> <br>
-				<div class="row">
-					<div class="col-md-4">
-						<p style="color: #F709AB; font-family: Georgia;"><strong>Nutriente</strong></p>
-						<label>Proteinas</label><br><br>
-						<label>Carbohidratos</label><br><br>
-						<label>Calorias</label>
-					</div>
-
-					<div class="col-md-4">
-						<p style="color: #F709AB; font-family: Georgia;""><strong>DV</strong></p>
-						<label>3.2 GR</label><br><br>
-						<label>4.5 GR</label><br><br>
-						<label>6.7 GR</label>
-					</div>
-
-					<div class="col-md-4">
-						<p style="color: #F709AB; font-family: Georgia;""><strong>DV%</strong></p>
-						<label>5%</label><br><br>
-						<label>7%</label><br><br>
-						<label>10%</label>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
 </section>
+</div>
 
 <section>
 	<div class="contenedor">
@@ -114,10 +86,11 @@
 	</div>
 </section>
 
+<div class="container">
 <section class="row">
 	<div class="container">
 		<h2>Recetas presentadas</h2>
-			<div class="container panel panel-default">
+			<div class="container">
 				<div class="col-md-4 panel-body">
 					<div class="col-xs-12 FeaturedRecipies " >
 						<img src="<?php echo base_url('assets/sources/img/plato1.png'); ?>" width="100%" heigh="100px">
@@ -160,12 +133,7 @@
 	</div>		
 </section>
 
-<section>
-	<div class="container openingHours">
-			<h3 >Opening Hours</h3>
-			<p><small>Monday to Friday</small><br/>10am-10pm</p>
-			<p><small>Saturday to Sunday</small><br/>9am-11pm</p>
-	</div>
-</section>
+</div>
 
+<?php $this->load->view('FrontEnd/Global/horaApertura'); ?>
 <?php $this->load->view('FrontEnd/Global/Footer'); ?>
