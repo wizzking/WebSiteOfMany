0880edd69c708f2ed0a53f283af3be623c80acea
KireyAguilar <2015030281@upsin.edu.mx>
2018-03-21 12:14:22 -0700

    recipies

--- application/views/FrontEnd/Recipies.php
+++ application/views/FrontEnd/Recipies.php
@@ -1,55 +1,121 @@
 <?php $this->load->view('FrontEnd/Global/Header'); ?>
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/sources/css/Kirey.css');?>">
-<body>
-	<div class="titulo">
-		<h1>Recetas</h1>
+
+	<div class="header">
+		<img src="<?php echo base_url('assets/sources/img/primera.jpg'); ?>" width="100%" heigh="100px">
+		<img class="titleIm" src="<?php echo base_url('assets/sources/img/titulo.png'); ?>" width="100%" heigh="100px">
+	</div>
+
+	
+	<div id="container-menu">
+		<ul class="menu">
+			<li><img class="lineas" src="<?php echo base_url('assets/sources/img/menu.png'); ?>" width="50" heigh="50px">
+				<ul >
+					<li><a><img src="<?php echo base_url('assets/sources/img/1.png'); ?>" width="40%" heigh="40px">
+					BREACKFAST</a></li>
+					<li><a><img  src="<?php echo base_url('assets/sources/img/2.png'); ?>" width="40%" heigh="40px">
+					STARTER</a></li>
+					<li><a><img  src="<?php echo base_url('assets/sources/img/3.png'); ?>" width="40%" heigh="40px">
+					LUNCH</a></li>
+					<li><a><img  src="<?php echo base_url('assets/sources/img/4.png'); ?>" width="40%" heigh="40px">
+					DINNNER</a></li>
+					<li><a><img  src="<?php echo base_url('assets/sources/img/5.png'); ?>" width="40%" heigh="40px">
+					DISSERT</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+
+	<h2 class="subir">Our Spscial Recipies</h2>
+	<p class="cuerpo">Our restaurant has the recipes that highlight who we are, the great taste for food, you will find the best recipes in the house.</p>
+	<div class="central" >
+		<div id="cuadro1">
+			<div class="cuadroFoto1">
+				<img src="<?php echo base_url('assets/sources/img/li1.jpg'); ?>" width="100%" heigh="100px">
+			</div>
+			<div class="lado1">
+				<label class="titulos"><b>Salad</label></b><br>
+				<img src="<?php echo base_url('assets/sources/img/estrellas.png'); ?>"><br><br>
+				<label>It is a special dish of our restaurant that tastes the customer.</label><br>
+				<button class="boton1">DETALLES</button>
+			</div>
+		</div>	
+
+		<div>
+			<div class="cuadroFoto2">
+				<img src="<?php echo base_url('assets/sources/img/li2.jpg'); ?>" width="100%" heigh="100px">
+			</div>
+			<div class="lado2">
+				<label class="titulos"><b>Mashed Potatoes</label></b><br>
+				<img src="<?php echo base_url('assets/sources/img/estrellas.png'); ?>"><br><br>
+				<label>It is a special dish of our restaurant that tastes the customer.</label><br>
+				<button class="boton2">DETALLES</button>
+			</div>
+		</div>
+
+		<div>
+			<div class="cuadroFoto3">
+				<img src="<?php echo base_url('assets/sources/img/li3.jpg'); ?>" width="100%" heigh="100px">
+			</div>
+			<div class="lado3">
+				<label class="titulos"><b>Flutes</label></b>
+				<img src="<?php echo base_url('assets/sources/img/estrella2.png'); ?>"><br><br>
+				<label>It is a special dish of our restaurant that tastes the customer.</label><br>
+				<button class="boton3">DETALLES</button>
+			</div>
+		</div>
+
+		<div>
+			<div class="cuadroFoto4">
+				<img src="<?php echo base_url('assets/sources/img/li4.jpg'); ?>" width="100%" heigh="100px">
+			</div>
+			<div class="lado4">
+				<label class="titulos"><b>Meats</label></b><br>
+				<img src="<?php echo base_url('assets/sources/img/estrellas.png'); ?>"><br><br>
+				<label>It is a special dish of our restaurant that tastes the customer.</label><br>
+				<button class="boton4">DETALLES</button>
+			</div>
+		</div>
+
+		<div>
+			<div class="cuadroFoto5">
+				<img src="<?php echo base_url('assets/sources/img/li5.jpg'); ?>" width="100%" heigh="100px">
+			</div>
+			<div class="lado5">
+				<label class="titulos"><b>Cakes</label></b><br>
+				<img src="<?php echo base_url('assets/sources/img/estrellas.png'); ?>"><br><br>
+				<label>It is a special dish of our restaurant that tastes the customer.</label><br>
+				<button class="boton5">DETALLES</button>
+			</div>
+		</div>
+
+		<div>
+			<div class="cuadroFoto6">
+				<img src="<?php echo base_url('assets/sources/img/li6.jpg'); ?>" width="100%" heigh="100px">
+			</div>
+			<div class="lado6">
+				<label class="titulos"><b>Chiken</label></b><br>
+				<img src="<?php echo base_url('assets/sources/img/estrellas.png'); ?>"><br><br>
+				<label>It is a special dish of our restaurant that tastes the customer.</label><br>
+				<button class="boton6">DETALLES</button>
+			</div>
+		</div>
+	</div>
+
+<div>
+	<div class="horario">
+		<label><b>Opening Hours</label>
 	</div>	
-	<p>
-		
-	</p>
-	<h2>Nuestras Recetas Especiales</h2>
-	<div class="cuadroFoto1">
-		<label>imagen</label>
-	</div><br>
-	<div class="lado1">
-		
+	<div class="hor">
+		<label class="indent">Monday to Friday</label>
+		<label class="indent">Saturday to Sunday</label>
 	</div>
+	<dir class="hora">
+		<label class="indent">10am - 10pm</label>
+		<label class="indent">9am - 11pm</label>
+	</dir>	
+</div>	
 
-	<div class="cuadroFoto2">
-		
-	</div><br>
-	<div class="lado2">
-		
-	</div>
 
-	<div class="cuadroFoto3">
-		
-	</div><br>
-	<div class="lado3">
-		
-	</div>
-
-	<div class="cuadroFoto4">
-		
-	</div><br>
-	<div class="lado4">
-		
-	</div>
-
-	<div class="cuadroFoto5">
-		
-	</div><br>
-	<div class="lado5">
-		
-	</div>
-
-	<div class="cuadroFoto6">
-		
-	</div><br>
-	<div class="lado6">
-		
-	</div>
-			
-</body>
 
 <?php $this->load->view('FrontEnd/Global/Footer'); ?>
