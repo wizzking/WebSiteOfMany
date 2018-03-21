0e18c9631a47b6cd101cca4702523e2b29969536
Fernanda <2015030240@upsin.edu.mx>
2018-03-20 20:15:42 -0700

    contactus, footer y chef

--- application/views/FrontEnd/Chef.php
+++ application/views/FrontEnd/Chef.php
@@ -1,93 +1,88 @@
 <?php $this->load->view('FrontEnd/Global/Header'); ?>
 
 <div id="form1">
-	<div class="row">
-		<div class="container chefHead">
-			<div class="form-group">
-				<div class="col-md-offset-1">
-					<h3><b>Head Chef</b></h3>
-					<div class="headChef">
-						<img src="<?= base_url('assets/sources/img/chefHead.jpg'); ?>" width="500px" heigh="300px">
-						<div class="textSpeciality" style="background-image: url(../../assets/sources/img/fondo7.jpg);">
-								<h3><b>Chef Benito</b></h3>
-								<p><font color="gray">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</font></p>
-								<div class="social-networks">
-			                      <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-			                      <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
-			                      <a href="#" class="google"><i class="fa fa-google-plus"></i></a>
-		                    	</div>
-		                    	<div class="menu">
-	                    		<h3>Expertise in</h3>
-	                    		<div class="option" id="optionBreakfast">
-									<a href=""><img src="../../assets/sources/img/icon_breakfast.png" width="18px" height="18px"> BREAKFAST</a>
-								</div>
-								<div class="option" id="optionStarter">
-									<a href=""><img src="../../assets/sources/img/icon_pizza.png" width="18px" height="18px"> STARTER</a>
-								</div>
-								<div class="option" id="optionLunch">
-									<a href=""><img src="../../assets/sources/img/icon_lunch.png" width="18px" height="18px"> LUNCH</a>
-								</div>
-			                  </div>
+	<div class="container">
+		<div class="row">
+			<h3><b>Head Chef</b></h3>
+			<div class="col-sm-6"><img src="<?= base_url('assets/sources/img/chefhead.jpg'); ?>" width="540px" height="300px"></div>
+			<div class="col-sm-6" id="fondoLeft">
+				<h3><b>Chef Benito</b></h3>
+					<p><font color="gray">Lorem ipsum dolor sit amet, consectetur adipisicing elit</font></p>
+					<div class="social-networks">
+                      <a href="#" class="twitter"> <img src="<?= base_url('assets/sources/img/icon_twitter.png'); ?>"></a>
+                      <a href="#" class="facebook"><img src="<?= base_url('assets/sources/img/icon_facebook.png'); ?>"></a>
+                      <a href="#" class="google"><img src="<?= base_url('assets/sources/img/icon_google.png'); ?>"></a>
+                	</div>
+                	<div class="menu">
+                		<h3>Expertise in</h3>
+                		<div class="option" id="optionBreakfast">
+							<a href=""><img src="../../assets/sources/img/icon_breakfast.png" width="18px" height="18px"> BREAKFAST</a>
 						</div>
-					</div>
-				</div>	
+						<div class="option" id="optionStarter">
+							<a href=""><img src="../../assets/sources/img/icon_pizza.png" width="18px" height="18px"> STARTER</a>
+						</div>
+						<div class="option" id="optionLunch">
+							<a href=""><img src="../../assets/sources/img/icon_lunch.png" width="18px" height="18px"> LUNCH</a>
+						</div>
+                  </div>
 			</div>
 		</div>
 	</div>
 </div>
 
-
-<div class="row" style="background-color: #e2f7e0;">
+<div class="form2" style="background-color: #eee">
 	<div class="container chefOptions">
-		<div class="form-group">
-			<div class="col-md-offset-1">
-				<h3><b>Chef Team</b></h3>
-				<div class="menu-gallery chef1">
-					<img src="<?= base_url('assets/sources/img/chefteam1.jpg'); ?>">
-					<div class="menu-desc ">
-					<h5><b><center>DOÑA CLARITA</center></b></h5>
+		<div class="row">
+			<div class="form-group">
+				<div class="col-md-offset-1 col-md-10">
+					<h3><b>Chef Team</b></h3>
+					<div class="menu-gallery chef1">
+						<img src="<?= base_url('assets/sources/img/chefteam1.jpg'); ?>">
+						<div class="menu-desc ">
+						<h5><b><center>DOÑA CLARITA</center></b></h5>
+						</div>
 					</div>
-				</div>
-				<div class="menu-gallery chef2">
-					<img src="<?= base_url('assets/sources/img/chefteam2.jpg'); ?>">
-					<div class="menu-desc ">
-					<h5><b><center>BERTHA</center></b></h5>
+					<div class="menu-gallery chef2">
+						<img src="<?= base_url('assets/sources/img/chefteam2.jpg'); ?>">
+						<div class="menu-desc ">
+						<h5><b><center>BERTHA</center></b></h5>
+						</div>
 					</div>
-				</div>
-				<div class="menu-gallery chef3">
-					<img src="<?= base_url('assets/sources/img/chefteam3.jpg'); ?>">
-					<div class="menu-desc ">
-					<h5><b><center>BERTHA</center></b></h5>
+					<div class="menu-gallery chef3">
+						<img src="<?= base_url('assets/sources/img/chefteam3.jpg'); ?>">
+						<div class="menu-desc ">
+						<h5><b><center>BERTHA</center></b></h5>
+						</div>
 					</div>
-				</div>
-				<div class="menu-gallery chef4">
-					<img src="<?= base_url('assets/sources/img/chefteam4.jpg'); ?>">
-					<div class="menu-desc ">
-					<h5><b><center>BERTHA</center></b></h5>
+					<div class="menu-gallery chef4">
+						<img src="<?= base_url('assets/sources/img/chefteam4.jpg'); ?>">
+						<div class="menu-desc ">
+						<h5><b><center>BERTHA</center></b></h5>
+						</div>
 					</div>
-				</div>
-				<div class="menu-gallery chef1">
-					<img src="<?= base_url('assets/sources/img/chefteam1.jpg'); ?>" >
-					<div class="menu-desc ">
-					<h5><b><center>DOÑA CLARITA</center></b></h5>
+					<div class="menu-gallery chef1">
+						<img src="<?= base_url('assets/sources/img/chefteam1.jpg'); ?>" >
+						<div class="menu-desc ">
+						<h5><b><center>DOÑA CLARITA</center></b></h5>
+						</div>
 					</div>
-				</div>
-				<div class="menu-gallery chef2">
-					<img src="<?= base_url('assets/sources/img/chefteam2.jpg'); ?>" >
-					<div class="menu-desc ">
-					<h5><b><center>BERTHA</center></b></h5>
+					<div class="menu-gallery chef2">
+						<img src="<?= base_url('assets/sources/img/chefteam2.jpg'); ?>" >
+						<div class="menu-desc ">
+						<h5><b><center>BERTHA</center></b></h5>
+						</div>
 					</div>
-				</div>
-				<div class="menu-gallery chef2">
-					<img src="<?= base_url('assets/sources/img/chefteam3.jpg'); ?>" >
-					<div class="menu-desc ">
-					<h5><b><center>BERTHA</center></b></h5>
+					<div class="menu-gallery chef2">
+						<img src="<?= base_url('assets/sources/img/chefteam3.jpg'); ?>" >
+						<div class="menu-desc ">
+						<h5><b><center>BERTHA</center></b></h5>
+						</div>
 					</div>
-				</div>
-				<div class="menu-gallery chef2">
-					<img src="<?= base_url('assets/sources/img/chefteam4.jpg'); ?>" >
-					<div class="menu-desc ">
-					<h5><b><center>BERTHA</center></b></h5>
+					<div class="menu-gallery chef2">
+						<img src="<?= base_url('assets/sources/img/chefteam4.jpg'); ?>" >
+						<div class="menu-desc ">
+						<h5><b><center>BERTHA</center></b></h5>
+						</div>
 					</div>
 				</div>
 			</div>
--- application/views/FrontEnd/ContactUs.php
+++ application/views/FrontEnd/ContactUs.php
@@ -19,17 +19,17 @@
 					<span class="glyphicon glyphicon-earphone"></span>
 					<h6><b>PHONE</b></h6>
 					<h6 style="color: gray;">+91 855 558 659</h6>
-					<h6 style="color: gray;"><b>EMAIL:</b>info@luxury.com</h6>
+					<h6 style="color: gray;"><b>EMAIL: </b>info@luxury.com</h6>
 				</div>
 			</div>
 			<div class="col-sm-4">
 				<div class="col-sm-8">
-					<span class="fa fa-share"></span>
+					<img src="<?= base_url('assets/sources/img/icon_share.png'); ?>">
 					<h6><b>FOLLOW US</b></h6>
 					<div class="social-networks">
-                      <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-                      <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
-                      <a href="#" class="google"><i class="fa fa-google-plus"></i></a>
+                      <a href="#" class="twitter"> <img src="<?= base_url('assets/sources/img/icon_twitter.png'); ?>"></a>
+                      <a href="#" class="facebook"><img src="<?= base_url('assets/sources/img/icon_facebook.png'); ?>"></a>
+                      <a href="#" class="google"><img src="<?= base_url('assets/sources/img/icon_google.png'); ?>"></a>
                     </div> 
 				</div>
 			</div>
@@ -42,13 +42,13 @@
 				<div class="form-group">
 					<div class="input-group input-group-sm">
 	                    <span class="input-group-addon form" id="sizing-addon3"><img src="<?= base_url('assets/sources/img/icon_user.png'); ?>" width="15px;"></span>
-	                    <input type="text" class="form-control" placeholder="Name*" aria-describedby="sizing-addon3">
+	                    <input type="text" id="input1" placeholder="Name*" aria-describedby="sizing-addon3">
 	                 </div>
 	            </div>
 	            <div class="form-group">
 	                 <div class="input-group input-group-sm">
 	                    <span class="input-group-addon form" id="sizing-addon3"><img src="<?= base_url('assets/sources/img/icon_msg.png'); ?>" width="15px;"></span>
-	                    <input type="text" class="form-control" placeholder="Message*" aria-describedby="sizing-addon3">
+	                    <input type="text" id="input2" placeholder="Message*" aria-describedby="sizing-addon3">
 	                 </div>
             	 </div>
             	 <div class="form-group">
@@ -59,7 +59,7 @@
 				<div class="form-group">
 					<div class="input-group input-group-sm">
 		                    <span class="input-group-addon form" id="sizing-addon3"><img src="<?= base_url('assets/sources/img/icon_carta.png'); ?>" width="15px;"></span>
-		                    <input type="text" class="form-control" placeholder="Email*" aria-describedby="sizing-addon3">
+		                    <input type="text" id="input3" placeholder="Email*" aria-describedby="sizing-addon3">
 		            </div>
 		        </div>
 			</div>
